c0ad95738203453993fea56423f707633dbba41c
Pavel Senin <seninp@gmail.com>
2015-01-08 13:03:19 +0100

    editing

--- RCode/ORG_Venn_count.R
+++ RCode/ORG_Venn_count.R
@@ -120,103 +120,19 @@ png(filename = "score_hits.png",width = 700, height = 650, pointsize = 12, type 
 grid.draw(venn.plot);
 dev.off();
 
-
 #============
-a31 <- n12345
-a30 <- n1234 - a31
-a29 <- n1235 - a31
-a28 <- n1245 - a31
-a27 <- n1345 - a31
-a26 <- n2345 - a31
-a25 <- n245 - a26 - a28 - a31
-a24 <- n234 - a26 - a30 - a31
-a23 <- n134 - a27 - a30 - a31
-a22 <- n123 - a29 - a30 - a31
-a21 <- n235 - a26 - a29 - a31
-a20 <- n125 - a28 - a29 - a31
-a19 <- n124 - a28 - a30 - a31
-a18 <- n145 - a27 - a28 - a31
-a17 <- n135 - a27 - a29 - a31
-a16 <- n345 - a26 - a27 - a31
-a15 <- n45 - a18 - a25 - a16 - a28 - a27 - a26 - a31
-a14 <- n24 - a19 - a24 - a25 - a30 - a28 - a26 - a31
-a13 <- n34 - a16 - a23 - a24 - a26 - a27 - a30 - a31
-a12 <- n13 - a17 - a22 - a23 - a27 - a29 - a30 - a31
-a11 <- n23 - a21 - a22 - a24 - a26 - a29 - a30 - a31
-a10 <- n25 - a20 - a21 - a25 - a26 - a28 - a29 - a31
-a9 <- n12 - a19 - a20 - a22 - a28 - a29 - a30 - a31
-a8 <- n14 - a18 - a19 - a23 - a27 - a28 - a30 - a31
-a7 <- n15 - a17 - a18 - a20 - a27 - a28 - a29 - a31
-a6 <- n35 - a16 - a17 - a21 - a26 - a27 - a29 - a31
-a5 <- area5 - a6 - a7 - a15 - a16 - a17 - a18 - a25 - a26 - 
-  a27 - a28 - a31 - a20 - a29 - a21 - a10
-a4 <- area4 - a13 - a14 - a15 - a16 - a23 - a24 - a25 - a26 - 
-  a27 - a28 - a31 - a18 - a19 - a8 - a30
-a3 <- area3 - a21 - a11 - a12 - a13 - a29 - a22 - a23 - a24 - 
-  a30 - a31 - a26 - a27 - a16 - a6 - a17
-a2 <- area2 - a9 - a10 - a19 - a20 - a21 - a11 - a28 - a29 - 
-  a31 - a22 - a30 - a26 - a25 - a24 - a14
-a1 <- area1 - a7 - a8 - a18 - a17 - a19 - a9 - a27 - a28 - 
-  a31 - a20 - a30 - a29 - a22 - a23 - a12
 
-#==========
-counts=c(a1+a2+a3+a4+a5, a6+a7+a8+a9+a10+a11+a12+a13+a14+a15, a16+a17+a18+a19+a20+a21+a22+a23+a24+a25, a26+a27+a28+a29+a30, a31)
-dfbars = data.frame(t(counts))
-names(dfbars)=paste(c(1:5))
-str(dfbars)
+common5 = intersect(blast_hits, intersect(intersect(last_hits, diamond_hits), intersect(lambda_hits, pauda_hits)))
 
-area1+area2+area3+area4+area5
-sum(dfbars[1,])
-dm=melt(dfbars)
+seq=paste("\"",paste(common5,collapse="\", \""),"\"",sep="")
+blast_hits = dbGetQuery(session, paste("select org_id, count(*) cnt from aligners_score where tag=\"BM\" and evalue<0.01 and org_id in (",
+                                       seq,") group by org_id order by cnt desc",sep=""))
+last_hits = dbGetQuery(session, paste("select org_id, count(*) cnt from aligners_score where tag=\"LAD\" and org_id in (",
+  seq,") group by org_id order by cnt desc",sep=""))
+diamond_hits = dbGetQuery(session, paste("select org_id, count(*) cnt from aligners_score where tag=\"DSM\" and org_id in (",
+                                         seq,") group by org_id order by cnt desc",sep=""))
+lambda_hits = dbGetQuery(session, paste("select org_id, count(*) cnt from aligners_score where tag=\"LSM\" and org_id in (",
+                                        seq,") group by org_id order by cnt desc",sep=""))
+pauda_hits = dbGetQuery(session, paste("select org_id, count(*) cnt from aligners_score where tag=\"PSM\" and org_id in (",
+                                       seq,") group by org_id order by cnt desc",sep=""))
 
-p=ggplot(dm, aes(x=variable, y=value, fill=variable)) + geom_bar(position="fill")
-p
-  geom_bar(stat='identity') +
-  coord_flip() + theme_bw()
-p
-
-
-#==========
-totals=c(area1, area2, area3, area4, area5)
-dftotals = data.frame(t(totals))
-names(dftotals)=c("BLAST","LAST","DIAMOND","LAMBDA","PAUDA")
-dm=melt(dftotals)
-p=ggplot(dm, aes(x=variable, y=value, fill=variable)) +
-  geom_bar(stat='identity') + theme_bw()
-p
-
-
-png(filename = "venn_hit_score_00.png",width = 800, height = 480);
-# start new page
-plot.new() 
-
-# setup layout
-gl <- grid.layout(nrow=1, ncol=2,width=1)
-# grid.show.layout(gl)
-
-# setup viewports
-vp.1 <- viewport(layout.pos.col=1, layout.pos.row=1) 
-vp.2 <- viewport(layout.pos.col=2, layout.pos.row=1) 
-
-pushViewport(viewport(layout=gl))
-
-# access the first position
-pushViewport(vp.1)
-
-# start new base graphics in first viewport
-grid.draw(venn.plot)
-
-# done with the first viewport
-popViewport()
-
-# move to the next viewport
-pushViewport(vp.2)
-
-# print our ggplot graphics here
-print(p, newpage = FALSE)
-
-# done with this viewport
-popViewport(1)
-
-#grid.draw(venn.plot);
-dev.off();
