022784bd111f7f47501e83c2f3d3282c1242e275
Pavel Senin <seninp@gmail.com>
2015-07-30 11:10:26 +0200

    adding the trimmer script

---
+++ Perl/trim_ns.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use Getopt::Long;
+
+my $maxn = "0.1";
+my $maxstretch = 4;
+
+my $result = GetOptions ("maxn=f" => \$maxn,
+                      "maxstretch=i"   => \$maxstretch);
+
+foreach my $line ( <STDIN> ) {
+    chomp( $line );
+    my $num_n = $line =~ tr/N//;
+    my $percentage = $num_n/length($line);
+    (my $alt_str = $line) =~ s/((N)+)/$2 . length($1)/ge;
+    my @all_nums = $alt_str =~ /(\d+)/g;
+    @all_nums = sort { $b <=> $a } @all_nums;
+    print "total Ns " . $num_n . " percentage " . $percentage . ", longest stretch " . $all_nums[0] . "\n";
+    if($percentage>$maxn){
+     print "percentage of Ns is above the threshold!\n";
+    }
+}
